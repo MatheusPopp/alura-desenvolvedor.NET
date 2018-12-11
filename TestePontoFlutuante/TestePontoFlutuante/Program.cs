0b76827b8a195384002a91771d35e50247118ada
Matheus Popp <matheus.popp@outlook.com>
2018-12-11 20:22:43 -0200

    Aula 6

---
+++ TestePontoFlutuante/TestePontoFlutuante/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestePontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 1200.50;
+
+            Console.WriteLine("Meu salário é: " + salario);
+        }
+    }
+}
